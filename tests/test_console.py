717985c23cd8eacfd860ddbe1980653b866e04e3
Sam <s.olubode@alustudent.com>
2023-10-04 15:50:51 +0100

    update: console test

--- tests/test_console.py
+++ tests/test_console.py
@@ -40,3 +40,18 @@ class TestConsole(unittest.TestCase):
         self.assertIn(output1, output2)
         self.assertIn('California', output2)
         self.assertIn("[State]", self.stdout.getvalue())
+
+    def test_create_city_with_input(self):
+        """Tests create City command with input"""
+        with patch('sys.stdout', new=self.stdout):
+            self.console.onecmd("create State name=\"California\"")
+            output1 = self.stdout.getvalue().strip()
+            self.console.onecmd("create City state_id=\"{}\" name=\"{}\""
+                                .format(output1, "San_Francisco"))
+            city_id = self.stdout.getvalue().strip().split('\n')[
+                1].split(' ')[0]
+            self.console.onecmd("all City")
+            output3 = self.stdout.getvalue().strip()
+        self.assertIn(output2, output3)
+        self.assertIn('San Francisco', output3)
+        self.assertIn("[City] ({})".format(city_id), output3)

6716388ff3c07e5eff6a43020fd2871043bcb97f
Sam <s.olubode@alustudent.com>
2023-10-04 16:01:23 +0100

    chore: add db and Grants

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,7 @@
+-- Create hbnb_dev_db database and hbnb_dev user
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+-- add the user hbnb_dev with password 'hbnb_dev_pwd'
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED WITH mysql_native_password 'hbnb_dev_pwd';
+-- add privileges to hbnb_dev
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';

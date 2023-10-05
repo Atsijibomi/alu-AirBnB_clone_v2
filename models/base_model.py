60e41d7bbef3e439840b1b3ce27a7b78222043c5
Sam <s.olubode@alustudent.com>
2023-10-05 12:03:53 +0100

    fix: relation compatibility

--- models/base_model.py
+++ models/base_model.py
@@ -3,7 +3,7 @@
 import uuid
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, String, DateTime, Integer
 
 Base = declarative_base()
 
@@ -11,7 +11,8 @@ Base = declarative_base()
 class BaseModel:
     """A base class for all hbnb models"""
 
-    id = Column(Integer, primary_key=True)
+    id = Column(String(60), primary_key=True)
+    # id = Column(Integer, primary_key=True)
     created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
     updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
 

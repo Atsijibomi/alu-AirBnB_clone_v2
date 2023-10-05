efc3372a831c45545760d7eec6964160c1a31063
Sam <s.olubode@alustudent.com>
2023-10-05 10:24:41 +0100

    chore: a whole lot of stuff at once

--- models/state.py
+++ models/state.py
@@ -1,8 +1,26 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+import models
+from models.base_model import BaseModel, Base
+from models import FileStorage, DBStorage, storage
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel):
     """ State class """
-    name = ""
+
+    __tablename__ = "states"
+
+    name = Column(String(128), nullable=False)
+
+    if isinstance(storage, DBStorage):
+        cities = relationship('City', back_populates="State",
+                              cascade="all, delete, delete-orphan")
+    if isinstance(storage, FileStorage):
+        @property
+        def cities(self):
+            """ Returns the list of City instances with state_id
+            equals to the current State.id. """
+            return [city for city in models.storage.all('City').values()
+                    if city.state_id == self.id]

334b401b972dfb0ec5acaf59897662041ad6ea7e
Sam <s.olubode@alustudent.com>
2023-10-05 10:56:49 +0100

    chore: sqlalchemy stuff

--- models/city.py
+++ models/city.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
-from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
-
-Base = declarative_base()
 
 
 class City(BaseModel, Base):

4b9e6e067759e5716c3395c0d1b8cc99b78d6010
Sam <s.olubode@alustudent.com>
2023-10-05 12:27:36 +0100

    fix: relation compatibility

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -27,6 +27,7 @@ class DBStorage:
         database = os.getenv('HBNB_MYSQL_DB')
         env = os.getenv('HBNB_ENV')
 
+        # To test locally
         # connection_url = 'mysql+pymysql://{}:{}@{}:3306/{}'.format(
         #     user, password, host, database)
         connection_url = 'mysql+mysqldb://{}:{}@{}:3306/{}'.format(

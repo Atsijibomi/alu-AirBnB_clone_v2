d19a46d82cfdb3e748d533cfea406696b72cffa5
Sam <s.olubode@alustudent.com>
2023-10-05 14:15:40 +0100

    modify cities

--- models/state.py
+++ models/state.py
@@ -15,7 +15,7 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
 
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship('City', back_populates="state",
+        cities = relationship('City', backref="state",
                               cascade="all, delete, delete-orphan")
     else:
         @property

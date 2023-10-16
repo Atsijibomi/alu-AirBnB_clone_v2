51a8332fd5aa5593d631a2e2a0bd3b20fa646534
Sam <s.olubode@alustudent.com>
2023-10-16 12:15:07 +0100

    Display HBNB

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -6,10 +6,10 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """Hello HBNB """
-    return 'Hello HBNB!'
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Display HBNB """
+    return 'HBNB'
 
 
 if __name__ == '__main__':

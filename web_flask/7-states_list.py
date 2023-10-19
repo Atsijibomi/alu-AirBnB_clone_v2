00a4064efad7e76d86e308861cbff8d482e8a1b0
Sam <s.olubode@alustudent.com>
2023-10-19 16:15:23 +0100

    display list of states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """Display HTML list of states"""
-    states = storage.all(State).values()
+    states = storage.all('State').values()
     return render_template('7-states_list.html', states=states)
 
 
